5f5772662b161908ece597992ed2f136ba9a8d0f
Tau <taumasaire@gmail.com>
2024-07-08 20:34:18 +0200

    adding the routes that needs auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,19 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method that returns False - path and excluded_paths will not be used
         """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+
+        # normalise the path by removing trailing slashes
+        normalized_path = path.rstrip('/')
+
+        # normalise the excluded paths
+        norm_excluded_paths = [path.rstrip('/') for path in excluded_paths]
+
+        if normalized_path in norm_excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Method that returns None - request will not be used
